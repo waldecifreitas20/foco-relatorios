b9733dca0b27e66d8a171f5c6145a195efec7a5f
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-01-13 01:42:16 -0300

    refactor: use single function to count orders

--- src/components/ViewContainer.tsx
+++ src/components/ViewContainer.tsx
@@ -3,12 +3,16 @@ import { type PropsWithChildren } from "react";
 
 interface ViewContainerProps extends PropsWithChildren {
   title: string;
+  subtitle?: string;
 }
 
 export function ViewContainer(props: ViewContainerProps) {
   return (
     <div className="w-[90%] mx-auto text-slate-700 font-medium">
-      <h2 className="text-4xl my-10 font-medium">{props.title}</h2>
+      <div className="my-10">
+        <h2 className="text-4xl font-medium mb-1">{props.title}</h2>
+        <p className="text-black/50 font-normal text-xs">{props.subtitle}</p>
+      </div>
       {props.children}
     </div>
   );
--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { ViewContainer } from "../components/ViewContainer";
 import { OrderContext } from "../provider/OrderContext";
+import type { ServiceStatus } from "../types/ServiceStatus";
+import type { Service } from "../types/Service";
 
 export function Dashboard() {
   const todayDate = new Date(Date.now()).toLocaleDateString();
@@ -9,33 +11,36 @@ export function Dashboard() {
 
   const generalStats = [
     { label: "Total", value: orders.length },
-    { label: "Cancelamentos", value: orders.filter(o => o.status === "Cancelado").length },
-    { label: "Veículos em Base", value: orders.filter(o => o.status === "Em base").length },
-    { label: "Atendimentos Finalizados", value: orders.filter(o => o.status === "Concluído").length },
+    { label: "Cancelamentos", value: countOrders("status", "Cancelado") },
+    { label: "Veículos em Base", value: countOrders("status", "Em base") },
+    { label: "Atendimentos Finalizados", value: countOrders("status", "Concluído") },
   ];
 
   const mainServices = [
-    { label: "Guincho", value: orders.filter(o => o.service === "Guincho").length },
-    { label: "Recarga de Bateria", value: orders.filter(o => o.service === "Recarga de Bateria").length },
-    { label: "Troca de Bateria", value: orders.filter(o => o.service === "Troca de Bateria").length },
+    { label: "Guincho", value: countOrders("service", "Guincho") },
+    { label: "Recarga de Bateria", value: countOrders("service", "Recarga de Bateria") },
+    { label: "Troca de Bateria", value: countOrders("service", "Troca de Bateria") },
   ];
 
   const otherServices = [
-    { label: "Chaveiro", value: orders.filter(o => o.service === "Chaveiro").length },
-    { label: "Desatolamento", value: orders.filter(o => o.service === "Desatolamento").length },
-    { label: "Troca de Pneu", value: orders.filter(o => o.service === "Troca de Pneu").length },
-    { label: "Pane Seca", value: orders.filter(o => o.service === "Pane Seca").length },
+    { label: "Chaveiro", value: countOrders("service", "Chaveiro") },
+    { label: "Desatolamento", value: countOrders("service", "Desatolamento") },
+    { label: "Troca de Pneu", value: countOrders("service", "Troca de Pneu") },
+    { label: "Pane Seca", value: countOrders("service", "Pane Seca") },
   ]
+
+  function countOrders(key: "service" | "status", value: ServiceStatus | Service) {
+    return orders.filter(o => o[key] === value).length;
+  }
 
   return (
     <>
-      <ViewContainer title="Visão Geral">
+      <ViewContainer title="Visão Geral" subtitle={`Estatísticas da data ${todayDate}`}>
         {/* day's summary */}
-
         <div className="bg-white mb-10 border shadow-lg rounded-lg flex flex-wrap py-4 xl:flex-nowrap justify-center w-full ">
           {generalStats.map((stat) => {
             return (
-              <div className="block w-full w-full border-l p-2 text-center">
+              <div className="block w-full border-l p-2 text-center">
                 <p>{stat.label}</p>
                 <p className="text-4xl text-red-600">{stat.value}</p>
               </div>
