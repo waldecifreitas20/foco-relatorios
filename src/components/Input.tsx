a5369c9e8a9b137d6f6bb752e75425ef3590afad
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-01-15 11:13:44 -0300

    feat: add disable behavior to input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,26 +2,34 @@ interface InputProps {
   type?: string;
   placeholder?: string;
   name?: string;
-  value?:string;
+  value?: string;
   label?: string;
+  readOnly?: boolean;
   onClick?: () => void;
 }
 
 export function Input(props: InputProps) {
   return (
     <div className="w-full">
-      {props.label && <p>{props.label}: </p>}
+      {props.label && (
+        <p>
+          {props.label}:
+          {props.readOnly && <i className="ml-1 fa-solid fa-lock fa-xs"></i>}
+        </p>
+      )}
       <input
+        disabled={props.readOnly}
         name={props.name ?? ""}
         type={props.type ?? "text"}
-        value={props.value}
-        className="
-        bg-white 
+        defaultValue={props.value}
+        className={`
+        ${props.readOnly ? "bg-slate-100 cursor-not-allowed" : "bg-white "}
         border border-neutral-200 
         w-full block 
         px-4 py-3 
         shadow-lg rounded-lg 
-        outline-none"
+        outline-none
+          `}
         placeholder={props.placeholder ?? ""}
         onClick={() => props.onClick!()}
       />
