134eab51119bda5451e47cb66e705c1dfb383ee4
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-01-27 03:22:44 +0000

    feat: loading fallback

---
+++ src/components/LoadingFallback.tsx
@@ -0,0 +1,9 @@
+export function LoadingFallback() {
+	return (
+		<p className="
+		w-full select-none 
+		text-slate-400 font-normal 
+		text-2xl text-center mt-50
+		">Carregando dados...</p>	
+	);
+}
