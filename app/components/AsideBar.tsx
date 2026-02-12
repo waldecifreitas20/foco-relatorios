5b988ffd29e066367fa558ad69c79c6340562963
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-12 08:33:38 +0000

    style: use 2 tab size

--- app/components/AsideBar.tsx
+++ app/components/AsideBar.tsx
@@ -1,9 +1,7 @@
-import type { Route } from "../+types/root";
-
 export default function AsideBar() {
-    return (
-        <aside>
-            <h1>Bosch</h1>
-        </aside>
-    );
+  return (
+    <aside>
+        <h1>Bosch</h1>
+    </aside>
+  );
 }
