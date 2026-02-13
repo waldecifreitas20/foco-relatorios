4a8a1a2cdabcf110f27ed5c3ef3cbe0fc29fcd85
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-13 03:21:47 +0000

    feat: update statuses

--- app/types/ServiceStatus.ts
+++ app/types/ServiceStatus.ts
@@ -1,13 +1,8 @@
 export type ServiceStatus = 
 
-"Em andamento" | 
-"Em base" | 
-"Aguardando direcionamento" | 
-"Aguardando aprovação de orçamento" | 
-"Aguardando confirmação de entrega" | 
-"Aguardando confirmação de Conclusão" | 
-"Aguardando confirmação de remoção" | 
-"Cancelado" | 
-"Serviço frustrado" | 
-"Agendado" | 
-"Concluído" 
+"Acionado" | 
+"Em Deslocamento" | 
+"Na Origem" | 
+"Em Base" | 
+"Concluído" | 
+"Cancelado"; 
