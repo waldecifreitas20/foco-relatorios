32905c7ca1ed063884f937458176daa72afb6270
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-13 07:34:21 +0000

    feat: basic home page

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,14 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
+
+export function meta({ }: Route.MetaArgs) {
+  return [
+    { title: "Monitoramento RSA | BOSCH" },
+    { name: "description", content: "Monitoramento de Roadside Assitance" },
+  ];
+}
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,19 +2,16 @@ import AsideBar from "~/components/AsideBar";
 import type { Route } from "./+types/home";
 
 
-export function meta({ }: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
 export default function Home() {
   return (
     <main className="flex p-4">
       <AsideBar />
-      <section className="max-w-[1000px]">
-        <h1>Dashboard</h1>
+      <section className="max-w-[1000px] px-4">
+        <h1 className="text-3xl font-semibold text-neutral-800">Painel de Monitoramento</h1>
+
+        <section>
+          
+        </section>
       </section>
     </main>
   );
