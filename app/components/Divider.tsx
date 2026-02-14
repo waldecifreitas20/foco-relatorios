e7e4b57280efbf17ccd8d86ee12f507ed6b17c4a
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-14 04:45:06 +0000

    refactor: use dynamic space to divider

--- app/components/Divider.tsx
+++ app/components/Divider.tsx
@@ -1,4 +1,14 @@
-export function Divider() {
-  return <div className="border-b border-neutral-200 block w-full my-8
-  "></div>
+interface DividerProps {
+  spaceTop?: number;
+  spaceBottom?: number;
+}
+
+export function Divider({ spaceTop = 2, spaceBottom = 2 }: DividerProps) {
+  return <div className="border-b border-neutral-200 block w-full
+  "
+    style={{
+      marginTop: `${spaceTop}rem`,
+      marginBottom: `${spaceBottom}rem`,
+    }}
+  ></div>
 }
