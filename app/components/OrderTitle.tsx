a6c3ccb64874747d9cebb9fc2db4eb89112294eb
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-19 18:01:26 -0300

    refactor: order tile component

---
+++ app/components/OrderTitle.tsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router";
+import type { Order } from "~/types/Order";
+
+interface OrderTitleProps {
+  order: Order;
+  linkTo: string;
+  trailing: React.ReactNode;
+}
+
+export function OrderTitle({ order, linkTo, trailing }: OrderTitleProps) {
+
+  function getDate(date: Date | undefined) {
+    return new Date(date ?? new Date(Date.now()))
+      .toLocaleString("pt-BR", {
+        day: "2-digit",
+        month: "short",
+        year: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit"
+      });
+  }
+
+  return (
+    <Link
+      to={linkTo}
+      key={order.ticket}
+      className="
+      grid grid-cols-7 items-center
+      py-4
+      text-sm text-center  hover:ml-1
+      border-l-4 border-transparent
+      bg-white hover:border-red-500 hover:bg-slate-100 text-slate-800
+    ">
+      <span>{order.plate}</span>
+      <span>{order.service}</span>
+      <span>{order.client}</span>
+      <span>{order.provider}</span>
+      <span>{order.ticket.substring(order.ticket.length - 7)}</span>
+      <span>{getDate(order.createdAt)} <i className="fa-regular fa-calendar"></i></span>
+      <span>
+        {trailing}
+      </span>
+    </Link>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -11,6 +11,7 @@ import { storageService } from "~/services/StorageService";
 import { useEffect } from "react";
 import { Link } from "react-router";
 import { PageTitle } from "~/components/PageTitle";
+import { OrderTitle } from "~/components/OrderTitle";
 
 
 export async function loader({ request }: Route.LoaderArgs) {
@@ -26,8 +27,8 @@ export async function loader({ request }: Route.LoaderArgs) {
   let orders = mock;
 
   if (filters.client) {
-      orders = orders.filter(o => {
-        return o.client
+    orders = orders.filter(o => {
+      return o.client
         .toLowerCase()
         .includes(filters.client!.toLowerCase())
     });
@@ -59,86 +60,66 @@ export default function Home({ loaderData }: Route.ComponentProps) {
             </Link>
           </span>
 
-        <UpdateDataButton />
-      </section>
+          <UpdateDataButton />
+        </section>
 
 
-      <section className="w-full overflow-hidden">
-        <ul className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 justify-between gap-4 block w-full">
-          {(["Acionado", "Agendado", "Em Base", "Concluído", "Cancelado"] as ServiceStatus[]).map(status => {
+        <section className="w-full overflow-hidden">
+          <ul className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 justify-between gap-4 w-full">
+            {(["Acionado", "Agendado", "Em Base", "Concluído", "Cancelado"] as ServiceStatus[]).map(status => {
+              return (
+                <Card>
+                  <p className="font-semibold text-sm text-slate-500 uppercase">{status}</p>
+                  <p className="text-6xl font-semibold text-slate-800 my-6">{orders.filter(o => o.status === status).length}</p>
+                </Card>
+              );
+            })}
+          </ul>
+        </section>
+
+
+        <section className="flex flex-col gap-5 my-4">
+          {(["Acionado", "Em Deslocamento", "Agendado", "Em Base", "Concluído", "Cancelado"] as ServiceStatus[]).map(status => {
+            const filteredOrders = orders.filter(o => o.status === status);
+
+
             return (
-              <Card>
-              <p className="font-semibold text-sm text-slate-500 uppercase">{status}</p>
-              <p className="text-6xl font-semibold text-slate-800 my-6">{orders.filter(o => o.status === status).length}</p>
-              </Card>
+              <Accordeon
+                disabled={filteredOrders.length === 0}
+                title={
+                  <span className="flex gap-6 items-center">
+                    {status}
+                    <Badge>Quantidade: {filteredOrders.length}</Badge>
+                  </span>
+                }>
+
+                {filteredOrders.map(o => {
+
+                  const getChecklistIcon = () => o.hasChecklist ?
+                    <i className="fa-solid text-green-400 fa-circle-check"></i>
+                    : <i className="fa-solid text-red-400 fa-xmark"></i>
+
+                  const getEta = () => (o.eta ? `${o.eta} minutos` : "Sem prévia");
+
+                  return (
+                    <OrderTitle
+                      linkTo={`/rsa/${o.plate}/${o.ticket}`}
+                      key={o.ticket}
+                      order={o}
+                      trailing={
+                        status === "Concluído" ?
+                          getChecklistIcon() :
+                          (<>{getEta()} <i className="fa-solid fa-hourglass-half"></i></>)
+                      } />
+                  );
+                })}
+              </Accordeon>
             );
-      })}
-    </ul>
-  </section>
+          })}
+        </section>
 
 
-  <section className="flex flex-col gap-5 my-4">
-    {(["Acionado", "Em Deslocamento", "Agendado", "Em Base", "Concluído", "Cancelado"] as ServiceStatus[]).map(status => {
-      const filteredOrders = orders.filter(o => o.status === status);
-
-
-      return (
-        <Accordeon
-        disabled={filteredOrders.length === 0}
-        title={
-          <span className="flex block gap-6 items-center">
-            {status}
-          <Badge>Quantidade: {filteredOrders.length}</Badge>
-          </span>
-        }>
-        {filteredOrders.map(o => {
-
-          const getChecklistIcon = () => o.hasChecklist ?
-        <i className="fa-solid text-green-400 fa-circle-check"></i>
-        : <i className="fa-solid text-red-400 fa-xmark"></i>
-
-          const getEta = () => (o.eta ? `${o.eta} minutos` : "Sem prévia");
-
-          return (
-            <Link
-            to={`/acionamentos/${o.plate}`}
-            key={o.ticket}
-            className="
-            grid grid-cols-7 items-center
-            py-4
-            text-sm text-center  hover:ml-1
-            border-l-4 border-transparent
-            bg-white hover:border-red-500 hover:bg-slate-100 text-slate-800
-            ">
-          <span>{o.plate}</span>
-          <span>{o.service}</span>
-          <span>{o.client}</span>
-          <span>{o.provider}</span>
-          <span>{o.ticket.substring(o.ticket.length - 7)}</span>
-            <span>{`${new Date(o.createdAt ?? new Date(Date.now()))
-            .toLocaleString("pt-BR", {
-                day: "2-digit",
-                month: "short",
-                year: "2-digit",
-                hour: "2-digit",
-                minute: "2-digit"
-        })}`} <i className="fa-regular fa-calendar"></i>
-        </span>
-        <span>
-          {status === "Concluído" ?
-          getChecklistIcon() :
-        (<>{getEta()} <i className="fa-solid fa-hourglass-half"></i></>)}
-        </span>
-      </Link>
-    );
-})}
-</Accordeon>
-);
-})}
-</section>
-
-
-</main>
-</div>
-);
+      </main>
+    </div>
+  );
 }
