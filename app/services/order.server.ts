841aef44010ae311837dab5353a01c33057dc5c9
Waldeci Freitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-23 06:38:28 +0000

    fix: server code  leaking to client

--- app/services/order.server.ts
+++ app/services/order.server.ts
@@ -1,22 +1,30 @@
 import axios from "axios";
 import type { Order } from "~/types/Order";
 
-
-
 const api = axios.create({
   baseURL: process.env.API_URL,
 });
 
-async function getAll(date?: string, page=0): Promise<{
+async function getAll(
+  date?: string,
+  page = 0
+): Promise<{
   status: number;
   orders: Order[];
   page: number;
 }> {
   const { data } = await api.get("/all");
-  
+
+  return data;
+}
+
+async function create(order:Order) {
+  const {data} = await api.post("/create", order);
+
   return data;
 }
 
 export const orderService = {
   getAll,
+  create
 };
