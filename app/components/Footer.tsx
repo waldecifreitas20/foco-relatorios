0c9784ff9c81ee566d4b4d0e1b6f5fa132ca1397
Waldeci Freitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-28 08:19:24 +0000

    refactor: footer as component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,17 @@
+export function Footer() {
+  return (
+    <footer 
+      className="
+      mx-auto py-5 w-full border text-slate-600 text-sm font-medium text-center">
+      Powered by
+      <a
+        target="_blank"
+        href="https://waldecifreitas.vercel.app"
+        className="text-red-600"
+      >
+        {" "}
+        Waldeci Freitas
+      </a>
+    </footer>
+  );
+}
