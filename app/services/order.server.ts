3fed20012472941e105eaba0be2d68136e5fef4b
Waldeci Freitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-28 07:26:32 +0000

    refactor: optize search url params at order service

--- app/services/order.server.ts
+++ app/services/order.server.ts
@@ -5,35 +5,39 @@ const api = axios.create({
   baseURL: process.env.API_URL,
 });
 
-async function getAll(
-  date?: string,
-  anyDate = false,
-  page = 0,
-): Promise<{
+
+
+async function getAll(date?: string,page = 0): Promise<{
   status: number;
   orders: Order[];
   page: number;
 }> {
-  const dateParam = date ? `createdAt=${date}&limit=200` : "";
-  const searchParams = anyDate ? "anyDate=true" : dateParam;
+  const searchParams = `createdAt=${date}&${page}&limit=200`;
   const { data } = await api.get(`/all?${searchParams}`);
-
   return data;
 }
+
+
 
 async function getOrder(ticket: string) {
   return await api.get(`/order/${ticket}`).then(response => response.data);
 }
 
+
+
 async function create(order:Order) {
-  const {data} = await api.post("/create", order);
+  const { data } = await api.post("/create", order);
   return data;
 }
 
+
+
 async function update(orderPatch: Partial<Order>) {
-  const {data} = await api.patch(`/update/${orderPatch.ticket}`, orderPatch);
+  const { data } = await api.patch(`/update/${orderPatch.ticket}`, orderPatch);
   return data;
 }
+
+
 
 export const orderService = {
   getAll,
