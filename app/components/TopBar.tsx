6e41b81251f52460701cbe556db5740215020b33
Waldeci Freitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-03-04 05:24:30 +0000

    temp: remove logos

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -6,32 +6,27 @@ import { useRouter } from "~/hooks/useRouter";
 
 export default function TopBar() {
   const { getPath } = useRouter();
-  const options: { label: string; path: string }[] = [
-    /*{ label: "Painel", path: "/" }, 
-    { label: "Reporte Diário", path: "/" },
-    { label: "Veículos em Base", path: "/" },
-    { label: "Agendamentos", path: "/" }, */
-  ];
+
   return (
     <>
       <header className="sticky top-0 border-b border-slate-200">
-        <img className="block w-screen h-2 object-cover" src={coloredBar} />
+        {/* 
+        OMITTED
+        <img className="block w-screen h-2 object-cover" src={coloredBar} /> 
+      */}
 
         <div className="bg-white px-4 py-4 flex gap-8 items-center justify-start border-b border-slate-100">
-          <Link to={getPath("/")}>
-            <img className="block mr-10" src={logo} alt="Bosch" />
+          <Link
+            to={getPath("/")}
+            className="text-slate-500 hover:text-slate-800"
+          >
+            {/* 
+            OMITTED
+            <img className="block mr-10" src={logo} alt="Bosch" /> 
+            */}
+            Página Inicial
           </Link>
 
-          {/* <nav className="w-fit h-full flex gap-8 items-center">
-            {options.map(op => {
-              return <Link
-                key={op.label}
-                to={op.path}
-                className="text-slate-500 hover:text-slate-900 block text-nowrap h-full leading-18">
-                {op.label}
-              </Link>
-            })}
-          </nav> */}
           <div className="w-1/2 max-w-[700px]">
             <Searchbar />
           </div>
